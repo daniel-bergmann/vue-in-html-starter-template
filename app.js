560803e24133044c4fa8696a85e267ac56833a9a
Daniel Bergmann <61001914+daniel-bergmann@users.noreply.github.com>
2021-10-05 21:18:53 +0000

    Update app.js

--- app.js
+++ app.js
@@ -10,22 +10,3 @@ const app = Vue.createApp({
 
 app.mount('#app')
 
-
-
-
-
-
-// const title = document.querySelector('h1');
-// const pic = document.querySelector('img');
-// getData();
-// async function getData() {
-//   const res = await fetch('http://localhost:5000/api');
-//   const data = await res.json();
-
-//   try {
-//     console.log(data);
-//     title.innerHTML = data.title;
-//   } catch {
-//     console.log('error...');
-//   }
-// }
